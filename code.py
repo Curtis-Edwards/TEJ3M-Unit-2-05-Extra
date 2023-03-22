d60027f5d31117b72ea62d8b1422c10e0dd63b90
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2023-03-22 17:20:45 -0400

    Backed up code

---
+++ code.py
@@ -0,0 +1,26 @@
+# Created by: Curtis Edwards
+# Created on: Mar 2023
+#
+# Rotates a servo motor for 180 degrees, then 180 in the opposite direction.
+
+
+"""CircuitPython Essentials Servo standard servo example"""
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.GP2, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+
+while True:
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+        
